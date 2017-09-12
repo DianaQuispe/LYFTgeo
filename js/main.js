e63cee512c94256f1e576cc2864f0c423fbb7079
DianaQuispe <danaalienmas@gmail.com>
2017-09-12 11:59:39 -0500

    añadiendo imagenes

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#contactForm')
+        .find('[name="phoneNumber"]')
+        .intlTelInput({
+            utilsScript: '/vendor/intl-tel-input/lib/libphonenumber/build/utils.js',
+            autoPlaceholder: true,
+            preferredCountries: ['fr', 'us', 'gb']
+        });
+
+    $('#contactForm')
+        .formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                phoneNumber: {
+                    validators: {
+                        callback: {
+                            message: 'The phone number is not valid',
+                            callback: function (value, validator, $field) {
+                                return value === '' || $field.intlTelInput('isValidNumber');
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        // Revalidate the number when changing the country
+        .on('click', '.country-list', function () {
+            $('#contactForm').formValidation('revalidateField', 'phoneNumber');
+        });
+});
