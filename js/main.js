fb6c811fdbb76ae6365b4d4fdeb7609a26941291
DianaQuispe <danaalienmas@gmail.com>
2017-09-12 18:09:26 -0500

    pusheo de index

--- js/main.js
+++ js/main.js
@@ -5,67 +5,37 @@ $('#next').click(function () {
     alert('g');
 })
 $(document).ready(function () {
-    // Define new validator
-    formValidation.Validator.intPhoneNumber = {
-        html5Attributes: {
-            message: 'message',
-            autoplaceholder: 'autoPlaceholder',
-            preferredcountries: 'preferredCountries',
-            utilsscript: 'utilsScript'
-        },
+    $('#contactForm')
+        .find('[name="phoneNumber"]')
+        .intlTelInput({
+            utilsScript: '/vendor/intl-tel-input/lib/libphonenumber/build/utils.js',
+            autoPlaceholder: true,
+            preferredCountries: ['fr', 'us', 'gb']
+        });
 
-        init: function (validator, $field, options) {
-            // Determine the preferred countries
-            var autoPlaceholder = options.autoPlaceholder === true || options.autoPlaceholder === 'true',
-                preferredCountries = options.preferredCountries || 'us';
-            if ('string' === typeof preferredCountries) {
-                preferredCountries = preferredCountries.split(',');
-            }
-
-            // Attach the intlTelInput on field
-            $field.intlTelInput({
-                utilsScript: options.utilsScript || '',
-                autoPlaceholder: autoPlaceholder,
-                preferredCountries: preferredCountries
-            });
-
-            // Revalidate the field when changing the country
-            var $form = validator.getForm(),
-                fieldName = $field.attr('data-fv-field');
-            $form.on('click.country.intphonenumber', '.country-list', function () {
-                $form.formValidation('revalidateField', fieldName);
-            });
-        },
-
-        destroy: function (validator, $field, options) {
-            $field.intlTelInput('destroy');
-
-            validator.getForm().off('click.country.intphonenumber');
-        },
-
-        validate: function (validator, $field, options) {
-            return $field.val() === '' || $field.intlTelInput('isValidNumber');
-        }
-    };
-
-    $('#contactForm').formValidation({
-        framework: 'bootstrap',
-        icon: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            phoneNumber: {
-                validators: {
-                    intPhoneNumber: {
-                        utilsScript: '/vendor/intl-tel-input/lib/libphonenumber/build/utils.js',
-                        autoPlaceholder: true,
-                        preferredCountries: 'fr,us,gb',
-                        message: 'The phone number is not valid'
+    $('#contactForm')
+        .formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                phoneNumber: {
+                    validators: {
+                        callback: {
+                            message: 'The phone number is not valid',
+                            callback: function (value, validator, $field) {
+                                return value === '' || $field.intlTelInput('isValidNumber');
+                            }
+                        }
                     }
                 }
             }
-        }
-    });
+        })
+        // Revalidate the number when changing the country
+        .on('click', '.country-list', function () {
+            $('#contactForm').formValidation('revalidateField', 'phoneNumber');
+        });
 });
